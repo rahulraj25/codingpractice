e530abeadffc3362c6d502a0e478948c7d49e33b
rahul raj <rahulrajvansh9708@gmail.com>
2021-12-26 23:03:15 +0530

    modified recursion fibonacci program

--- CodingPractice/src/miscellaneous/FibonacciUsingRecursion.java
+++ CodingPractice/src/miscellaneous/FibonacciUsingRecursion.java
@@ -1,18 +1,22 @@
 package miscellaneous;
 
 public class FibonacciUsingRecursion {
+
 	public static void main(String[] args) {
 		
-		fibonacciUsingRecursion(5);
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(fibonacciUsingRecursion(i) + " ");
+		}
 	}
 	
 		
 	public static int fibonacciUsingRecursion(int n){
-		System.out.println(n);
-			if(n == 0 || n == 1)
+		
+			if(n == 0 || n == 1){
 				return n;
-					
-			else
+			}
+			
+			else 
 				return fibonacciUsingRecursion(n-1)  + fibonacciUsingRecursion(n-2);
 		}
 }
